01a72776c88452bc227a99b39407033abf0fb558
marfoldi <marfoldi_@hotmail.com>
2015-03-31 17:42:00 +0200

    ShellSort is almost done

--- SRTNGLGRTHMS/src/srtnglgrthms/model/ShellSort.java
+++ SRTNGLGRTHMS/src/srtnglgrthms/model/ShellSort.java
@@ -2,14 +2,15 @@ package srtnglgrthms.model;
 
 import srtnglgrthms.controller.OverviewChartController;
 
-public class ShellSort extends SortingAlgorithm {
-	private static int j;
-	private static int gap;
-	private static int i;
+
+public class ShellSort extends SortingAlgorithm{
+	private static final int[] gapArray = {1750,701,301,132,57,23,10,4,1};
+	private static int gapIdx = selectGap();
+	private static int outerIndex = gapArray[gapIdx];
+	private static int innerIndex = 0;
+	private static boolean isSelected = false;
 	
-	private ShellSort() {
-		init();
-	}
+	private ShellSort() {}
 	
 	private static class SortHolder {
         private static final ShellSort INSTANCE = new ShellSort();
@@ -19,44 +20,88 @@ public class ShellSort extends SortingAlgorithm {
         return SortHolder.INSTANCE;
     }
     
-    private static void init() {
-    	j = 1;
-    	gap = SortingAlgorithm.getNumbers().length/2;
-    	i=gap;
-    }
-
-	public void step() {
-		if (gap > 0) {
-			if(i < SortingAlgorithm.getNumbers().length) {
-				j = i;
-				int temp = data.get(i).getYValue();
-				while (j >= gap && data.get(j-gap).getYValue() > temp) {
-					data.get(j).setYValue(data.get(j-gap).getYValue());
-					OverviewChartController.setColor(data.get(j).getNode(), "swap");
-					OverviewChartController.setColor(data.get(j-gap).getNode(), "swap");
-					j = j - gap;
-				}
-				data.get(j).setYValue(temp);
-				i++;
-			}
-			else {
-			if (gap == 2) {
-				gap = 1;
-				i=gap;
-				step();
-			} else {
-				gap *= (5.0 / 11);
-				i = gap;
-				step();
-			}
-			step();
-		}
-		}
-	}
-
-	@Override
 	public void setDefaults() {
-		// TODO Auto-generated method stub
-		
+		gapIdx = selectGap();
+		outerIndex = gapArray[gapIdx];
+		innerIndex = 0;
+		isSelected=false;
+		counterData.clear();
+		counterData.add(new CounterData("Összehasonlítások", 0));
+		counterData.add(new CounterData("Mozgatások", 0));
 	}
+	
+	private static int selectGap() {
+		for(int i=0; i<gapArray.length-1; ++i) {
+			if(data.size()>gapArray[i]) return i;
+		}
+		return 0;
+	}
+
+    @Override
+	public void step() {
+    	if(!isSelected) {
+    		OverviewChartController.setColor(data.get(outerIndex).getNode(), "select");
+    		isSelected=true;
+    	}
+    	else if(outerIndex<data.size()) {
+			if (data.get(innerIndex+gapArray[gapIdx]).getYValue() >= data.get(innerIndex).getYValue()) {	
+    			OverviewChartController.setColor(data.get(innerIndex).getNode(), "swap");
+    			OverviewChartController.setColor(data.get(innerIndex+gapArray[gapIdx]).getNode(), "select");
+    			isSelected=false;
+    			if(outerIndex<data.size()-1) {
+    				outerIndex++;
+        			innerIndex++;
+        			System.out.println("NÖVELTEM");
+    			}
+    			else if(gapIdx<gapArray.length-1) {
+    					System.out.println("GAPCSERE, MINDEN0");
+    					gapIdx++;
+    					outerIndex = gapArray[gapIdx];
+    					innerIndex = 0;
+    					setRestColor();
+    			}
+    		}
+    		else if(data.get(innerIndex+gapArray[gapIdx]).getYValue() < data.get(innerIndex).getYValue()) {
+    			System.out.println("SWAP");
+    			swap(innerIndex+gapArray[gapIdx], innerIndex);
+    			counterData.get(1).incValue();
+    			OverviewChartController.setColor(data.get(innerIndex).getNode(), "swap");
+    			OverviewChartController.setColor(data.get(innerIndex+gapArray[gapIdx]).getNode(), "select");
+    			if (innerIndex-gapArray[gapIdx]>=0 && data.get(innerIndex-gapArray[gapIdx]).getYValue() > data.get(innerIndex).getYValue())
+    				innerIndex=innerIndex-gapArray[gapIdx];
+    			else innerIndex = outerIndex-gapArray[gapIdx];
+    		}
+    		counterData.get(0).incValue();
+    	}
+    }
+	
+	private void setRestColor() {
+		for (int i = 0; i < data.size(); i++) {
+			OverviewChartController.setColor(data.get(i).getNode(), "default");
+		}
+	}
+	
+	public static Runnable sort = () -> {
+		int[] numbers = new int[SortingAlgorithm.getNumbers().length];
+		System.arraycopy( SortingAlgorithm.getNumbers(), 0, numbers, 0, SortingAlgorithm.getNumbers().length);
+	    int swapCounter = 0; //Increment this counter whenever a swap takes place
+	    int moveCounter=0; //Increment this counter whenever a movement takes place
+	    for (int i=0; i < numbers.length-1; i++)
+	    {
+	       int temp = numbers[i+1];
+	       int j;
+	       for(j=i; j>=0; --j) {
+	    	   moveCounter++;
+	    	   if(temp<numbers[j]) {
+	    		   swapCounter++;
+	    		   numbers[j+1] = numbers[j];
+	    	   }
+	    	   else {
+	    		   break;
+	    	   }
+	       }
+	       numbers[j+1] = temp;
+	    }
+	    benchmarkData.add(new BenchmarkData("Shell rendezés", moveCounter, swapCounter));
+	};
 }
